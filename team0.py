66f4dd8147c0c4d3862e199d902a2ec1442a6a38
cassiuscharak <57411734+cassiuscharak@users.noreply.github.com>
2019-11-14 12:14:22 -0600

    Update team0.py

--- team0.py
+++ team0.py
@@ -6,18 +6,17 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'GoldenBals' # Only 10 chars displayed.
+strategy_name = 'Maths'
+strategy_description = 'It has b as -1 and c as 1 and if positive it colludes and negative it betrays unless both peoples score is above 500 then it betrays.'
+import random 
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    first_move=[b,c]
+    random.first_move
     
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
+    'b'=-1
+    'c'=1    
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,9 +24,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -65,4 +61,6 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')          
+
+   
