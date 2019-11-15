f7daa36a1ac1ee08dc91c3c3c0dae05a802f853d
MitchellSchall <57368033+MitchellSchall@users.noreply.github.com>
2019-11-15 12:13:05 -0600

    rtjdfjfhjdfy

--- team11.py
+++ team11.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'cool guy' # Only 10 chars displayed.
+strategy_name = 'Tit for tat'
+strategy_description = 'Treat others the way you want to be treated'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,10 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if their_history[-1]=='c':
+        return 'c'
+    else:
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
