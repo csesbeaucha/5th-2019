8debf9b0412b379da9b6b098c5650078956af9a1
Tanner Sharon <57368020+slagesse@users.noreply.github.com>
2019-11-15 12:29:03 -0600

    Update team3.py

--- team3.py
+++ team3.py
@@ -22,7 +22,7 @@ def move(my_history, their_history, my_score, their_score):
         return 'c'
     if len(their_history) == 5:
         counter = 0
-        for i in len(their_history):
+        for i in range(5):
             if my_history[i] == their_history[i]:
                 counter += 1
             if counter == 5:
