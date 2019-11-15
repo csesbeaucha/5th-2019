cd0b59fdc582ca40c6a44d5e364f01d7a7048e37
z-hall <57368139+z-hall@users.noreply.github.com>
2019-11-15 11:57:07 -0600

    Update team12.py

--- team12.py
+++ team12.py
@@ -26,14 +26,12 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
     if their_score >= my_score:
-     return 'b'
-    elif: their_score != my_score:
-      return 'b'
+        return 'b'
     else: 
         return'c'
-         
+            
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
