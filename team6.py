1a5a333d6e2002d02a2f59be1a6ad6faaa90c821
0MaxWM0 <maxwm2002@gmail.com>
2019-11-18 11:50:56 -0600

    Update team6.py

--- team6.py
+++ team6.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team6' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Da Best Team' # Only 10 chars displayed.
+strategy_name = 'Yeet Yeet Try Me'
+strategy_description = 'You will never know...'
 import random
 
 def move(my_history, their_history, my_score, their_score):
